25811a3001d8f83794e0d0ca96eaf463fc5d806f
waylongo <wlwu1993@gmail.com>
2019-10-06 18:16:03 -0500

    first commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# IEEE-CIS Fraud Detection
+
+This is the repository for the Kaggle competition: 
+
+<https://www.kaggle.com/c/ieee-fraud-detection/overview>
+
+I got 177th out of 6381 teams (top 3%)
+
+![](imgs/result.png)
+
+The model I used is:
+light gbm
+
---
+++ models/utils.py
@@ -0,0 +1,366 @@
+########################### Helpers
+#################################################################################
+## -------------------
+## Seeder
+# :seed to make all processes deterministic     # type: int
+import numpy as np 
+import pandas as pd 
+import matplotlib.pylab as plt
+import sys, gc, warnings, random, math, time, datetime, os
+from tqdm import tqdm_notebook
+from sklearn import preprocessing, metrics
+from sklearn.model_selection import train_test_split, KFold, GroupKFold
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_absolute_error
+import xgboost as xgb
+import lightgbm as lgb
+from catboost import CatBoostClassifier
+
+def seed_everything(seed=0):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+## ------------------- 
+
+## -------------------
+## Memory Reducer
+# :df pandas dataframe to reduce size             # type: pd.DataFrame()
+# :verbose                                        # type: bool
+def reduce_mem_usage(df, verbose=True):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2    
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)    
+    end_mem = df.memory_usage().sum() / 1024**2
+    if verbose: print('Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction)'.format(end_mem, 100 * (start_mem - end_mem) / start_mem))
+    return df
+## -------------------
+
+def add_noise(series, noise_level):
+    return series * (1 + noise_level * np.random.randn(len(series)))
+
+def target_encode(trn_series=None, 
+                  tst_series=None, 
+                  target=None, 
+                  min_samples_leaf=1, 
+                  smoothing=1,
+                  noise_level=0):
+    """
+    Smoothing is computed like in the following paper by Daniele Micci-Barreca
+    https://kaggle2.blob.core.windows.net/forum-message-attachments/225952/7441/high%20cardinality%20categoricals.pdf
+    trn_series : training categorical feature as a pd.Series
+    tst_series : test categorical feature as a pd.Series
+    target : target data as a pd.Series
+    min_samples_leaf (int) : minimum samples to take category average into account
+    smoothing (int) : smoothing effect to balance categorical average vs prior  
+    """ 
+    assert len(trn_series) == len(target)
+    assert trn_series.name == tst_series.name
+    temp = pd.concat([trn_series, target], axis=1)
+    # Compute target mean 
+    averages = temp.groupby(by=trn_series.name)[target.name].agg(["mean", "count"])
+    # Compute smoothing
+    smoothing = 1 / (1 + np.exp(-(averages["count"] - min_samples_leaf) / smoothing))
+    # Apply average function to all target data
+    prior = target.mean()
+    # The bigger the count the less full_avg is taken into account
+    averages[target.name] = prior * (1 - smoothing) + averages["mean"] * smoothing
+    averages.drop(["mean", "count"], axis=1, inplace=True)
+    # Apply averages to trn and tst series
+    ft_trn_series = pd.merge(
+        trn_series.to_frame(trn_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=trn_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_trn_series.index = trn_series.index 
+    ft_tst_series = pd.merge(
+        tst_series.to_frame(tst_series.name),
+        averages.reset_index().rename(columns={'index': target.name, target.name: 'average'}),
+        on=tst_series.name,
+        how='left')['average'].rename(trn_series.name + '_mean').fillna(prior)
+    # pd.merge does not keep the index so restore it
+    ft_tst_series.index = tst_series.index
+    return add_noise(ft_trn_series, noise_level), add_noise(ft_tst_series, noise_level)
+
+## -------------------
+def make_predictions(tr_df, tt_df, y, features_columns, lgb_params, NFOLDS=6):
+
+    folds = GroupKFold(n_splits=NFOLDS)
+    split_groups = tr_df['DT_month']
+
+    X = tr_df[features_columns]
+    X_test = tt_df[features_columns]
+
+    pred_df = pd.DataFrame()
+    pred_df['TransactionID'] = tt_df.reset_index()['TransactionID']
+    pred_df['isFraud'] = np.zeros(len(pred_df))
+
+    predictions = np.zeros(len(tt_df))
+    oof = np.zeros(len(tr_df))
+
+    for fold_, (trn_idx,
+                val_idx) in enumerate(folds.split(X, y, groups=split_groups)):
+        tr_x, tr_y = X.iloc[trn_idx, :], y.iloc[trn_idx]
+        vl_x, vl_y = X.iloc[val_idx, :], y.iloc[val_idx]
+
+        print('Fold:', fold_, ' - ', len(tr_x), len(vl_x))
+        tr_data = lgb.Dataset(tr_x, label=tr_y)
+        vl_data = lgb.Dataset(vl_x, label=vl_y)
+
+        estimator = lgb.train(
+            lgb_params,
+            tr_data,
+            valid_sets=[tr_data, vl_data],
+            verbose_eval=100,
+        )
+        
+        pp_p = estimator.predict(X_test)
+        predictions += pp_p / NFOLDS
+
+        oof_preds = estimator.predict(vl_x)
+        oof[val_idx] = (oof_preds - oof_preds.min()) / (oof_preds.max() -
+                                                        oof_preds.min())
+
+        feature_imp = pd.DataFrame(sorted(zip(estimator.feature_importance(),X.columns)), columns=['Value','Feature'])
+        print(feature_imp)
+
+        del tr_x, tr_y, vl_x, vl_y, tr_data, vl_data
+        gc.collect()
+
+    pred_df['isFraud'] = predictions
+    pred_df = pred_df.set_index('TransactionID')
+
+    print('---------------------------------------')
+    print('OOF AUC:', metrics.roc_auc_score(y, oof))
+
+    return pred_df, metrics.roc_auc_score(y, oof)
+## -------------------???
+def make_predictions_cat(tr_df, tt_df, y, features_columns, cat_params, NFOLDS=6):
+
+    folds = GroupKFold(n_splits=NFOLDS)
+    split_groups = tr_df['DT_month']
+
+    X = tr_df[features_columns]
+    X_test = tt_df[features_columns]
+
+    pred_df = pd.DataFrame()
+    pred_df['TransactionID'] = tt_df.reset_index()['TransactionID']
+    pred_df['isFraud'] = np.zeros(len(pred_df))
+
+    predictions = np.zeros(len(tt_df))
+    oof = np.zeros(len(tr_df))
+
+    for fold_, (trn_idx,
+                val_idx) in enumerate(folds.split(X, y, groups=split_groups)):
+        tr_x, tr_y = X.iloc[trn_idx, :], y.iloc[trn_idx]
+        vl_x, vl_y = X.iloc[val_idx, :], y.iloc[val_idx]
+
+        print('Fold:', fold_, ' - ', len(tr_x), len(vl_x))
+        
+        estimator = CatBoostClassifier(**cat_params)  
+        estimator.fit(
+            X.iloc[trn_idx,:],y[trn_idx],
+            eval_set=(X.iloc[val_idx,:], y[val_idx]),
+            cat_features=categorical_features,
+            use_best_model=True,
+            verbose=True)
+        
+        oof_preds = estimator.predict_proba(X.iloc[val_idx,:])[:,1]
+        oof[val_idx] = (oof_preds - oof_preds.min())/(oof_preds.max() - oof_preds.min())
+        
+        pp_p = estimator.predict(X_test)
+        predictions += pp_p / NFOLDS
+
+        feature_imp = pd.DataFrame(sorted(zip(estimator.feature_importance(),X.columns)), columns=['Value','Feature'])
+        print(feature_imp)
+
+        del tr_x, tr_y, vl_x, vl_y, tr_data, vl_data
+        gc.collect()
+
+    pred_df['isFraud'] = predictions
+    pred_df = pred_df.set_index('TransactionID')
+
+    print('---------------------------------------')
+    print('OOF AUC:', metrics.roc_auc_score(y, oof))
+
+    return pred_df, metrics.roc_auc_score(y, oof)
+
+## -------------------
+def make_predictions_leave_fold_0(tr_df, tt_df, y, lgb_params, NFOLDS=6):
+
+    folds = GroupKFold(n_splits=NFOLDS)
+    split_groups = tr_df['DT_month']
+    X = tr_df.drop(['DT_month'], axis=1)
+
+    pred_df = pd.DataFrame()
+    pred_df['TransactionID'] = tt_df.reset_index()['TransactionID']
+    pred_df['isFraud'] = np.zeros(len(pred_df))
+
+    predictions = np.zeros(len(tt_df))
+    oof = np.zeros(len(tr_df))
+
+    for fold_, (trn_idx, val_idx) in enumerate(folds.split(X, y, groups=split_groups)):
+        
+        if(fold_ == 0):
+            print('folder 0 passed')
+            continue
+        
+        tr_x, tr_y = X.iloc[trn_idx, :], y.iloc[trn_idx]
+        vl_x, vl_y = X.iloc[val_idx, :], y.iloc[val_idx]
+
+        print('Fold:', fold_, ' - ', len(tr_x), len(vl_x))
+        tr_data = lgb.Dataset(tr_x, label=tr_y)
+        vl_data = lgb.Dataset(vl_x, label=vl_y)
+
+        estimator = lgb.train(
+            lgb_params,
+            tr_data,
+            valid_sets=[tr_data, vl_data],
+            verbose_eval=200,
+        )
+
+        pp_p = estimator.predict(tt_df)
+        predictions += pp_p / (NFOLDS - 1)
+
+        oof_preds = estimator.predict(vl_x)
+        oof[val_idx] = (oof_preds - oof_preds.min()) / (oof_preds.max() -
+                                                        oof_preds.min())
+
+        #         if LOCAL_TEST:
+        #             feature_imp = pd.DataFrame(sorted(zip(estimator.feature_importance(),X.columns)), columns=['Value','Feature'])
+        #             print(feature_imp)
+
+        del tr_x, tr_y, vl_x, vl_y, tr_data, vl_data
+        gc.collect()
+
+    pred_df['isFraud'] = predictions
+    pred_df = pred_df.set_index('TransactionID')
+
+    print('---------------------------------------')
+    print('OOF AUC:', metrics.roc_auc_score(y, oof))
+
+    return pred_df
+
+## -------------------
+    
+emails = {'gmail': 'google', 'att.net': 'att', 'twc.com': 'spectrum', 'scranton.edu': 'other', 'optonline.net': 'other',
+          'hotmail.co.uk': 'microsoft', 'comcast.net': 'other', 'yahoo.com.mx': 'yahoo', 'yahoo.fr': 'yahoo',
+          'yahoo.es': 'yahoo', 'charter.net': 'spectrum', 'live.com': 'microsoft', 'aim.com': 'aol', 'hotmail.de': 'microsoft',
+          'centurylink.net': 'centurylink', 'gmail.com': 'google', 'me.com': 'apple', 'earthlink.net': 'other', 
+          'gmx.de': 'other', 'web.de': 'other', 'cfl.rr.com': 'other', 'hotmail.com': 'microsoft', 'protonmail.com': 'other',
+          'hotmail.fr': 'microsoft', 'windstream.net': 'other', 'outlook.es': 'microsoft', 'yahoo.co.jp': 'yahoo',
+          'yahoo.de': 'yahoo', 'servicios-ta.com': 'other', 'netzero.net': 'other', 'suddenlink.net': 'other',
+          'roadrunner.com': 'other', 'sc.rr.com': 'other', 'live.fr': 'microsoft', 'verizon.net': 'yahoo',
+          'msn.com': 'microsoft', 'q.com': 'centurylink', 'prodigy.net.mx': 'att', 'frontier.com': 'yahoo',
+          'anonymous.com': 'other', 'rocketmail.com': 'yahoo', 'sbcglobal.net': 'att', 'frontiernet.net': 'yahoo',
+          'ymail.com': 'yahoo', 'outlook.com': 'microsoft', 'mail.com': 'other', 'bellsouth.net': 'other',
+          'embarqmail.com': 'centurylink', 'cableone.net': 'other', 'hotmail.es': 'microsoft', 'mac.com': 'apple',
+          'yahoo.co.uk': 'yahoo', 'netzero.com': 'other', 'yahoo.com': 'yahoo', 'live.com.mx': 'microsoft', 'ptd.net': 'other',
+          'cox.net': 'other', 'aol.com': 'aol', 'juno.com': 'other', 'icloud.com': 'apple'}
+us_emails = ['gmail', 'net', 'edu']
+
+## -------------------
+
+def setBrowser(df):
+    df['lastest_browser'] = np.zeros(df.shape[0])
+    df.loc[df["id_31"]=="samsung browser 7.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="opera 53.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="mobile safari 10.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="google search application 49.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="firefox 60.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="edge 17.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 69.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 67.0 for android",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 63.0 for android",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 63.0 for ios",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 64.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 64.0 for android",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 64.0 for ios",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 65.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 65.0 for android",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 65.0 for ios",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 66.0",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 66.0 for android",'lastest_browser']=1
+    df.loc[df["id_31"]=="chrome 66.0 for ios",'lastest_browser']=1
+    return df
+
+## -------------------
+    
+def setDevice(df):
+    df['DeviceInfo'] = df['DeviceInfo'].fillna('unknown_device').str.lower()
+    
+    df['device_name'] = df['DeviceInfo'].str.split('/', expand=True)[0]
+
+    df.loc[df['device_name'].str.contains('SM', na=False), 'device_name'] = 'Samsung'
+    df.loc[df['device_name'].str.contains('sam', na=False), 'device_name'] = 'Samsung'
+    df.loc[df['device_name'].str.contains('sm', na=False), 'device_name'] = 'Samsung'
+    df.loc[df['device_name'].str.contains('SAMSUNG', na=False), 'device_name'] = 'Samsung'
+    df.loc[df['device_name'].str.contains('GT-', na=False), 'device_name'] = 'Samsung'
+    df.loc[df['device_name'].str.contains('Moto G', na=False), 'device_name'] = 'Motorola'
+    df.loc[df['device_name'].str.contains('Moto', na=False), 'device_name'] = 'Motorola'
+    df.loc[df['device_name'].str.contains('moto', na=False), 'device_name'] = 'Motorola'
+    df.loc[df['device_name'].str.contains('LG-', na=False), 'device_name'] = 'LG'
+    df.loc[df['device_name'].str.contains('rv:', na=False), 'device_name'] = 'RV'
+    df.loc[df['device_name'].str.contains('HUAWEI', na=False), 'device_name'] = 'Huawei'
+    df.loc[df['device_name'].str.contains('ALE-', na=False), 'device_name'] = 'Huawei'
+    df.loc[df['device_name'].str.contains('ale', na=False), 'device_name'] = 'Huawei'
+    df.loc[df['device_name'].str.contains('-L', na=False), 'device_name'] = 'Huawei'
+    df.loc[df['device_name'].str.contains('Blade', na=False), 'device_name'] = 'ZTE'
+    df.loc[df['device_name'].str.contains('BLADE', na=False), 'device_name'] = 'ZTE'
+    df.loc[df['device_name'].str.contains('Linux', na=False), 'device_name'] = 'Linux'
+    df.loc[df['device_name'].str.contains('XT', na=False), 'device_name'] = 'Sony'
+    df.loc[df['device_name'].str.contains('HTC', na=False), 'device_name'] = 'HTC'
+    df.loc[df['device_name'].str.contains('ASUS', na=False), 'device_name'] = 'Asus'
+
+    df.loc[df.device_name.isin(df.device_name.value_counts()[df.device_name.value_counts() < 200].index), 'device_name'] = "Others"
+    gc.collect()
+    
+    return df
+
+## -------------------
+    
+def get_too_many_null_attr(data):
+    many_null_cols = [col for col in data.columns if data[col].isnull().sum() / data.shape[0] > 0.9]
+    return many_null_cols
+def get_too_many_repeated_val(data):
+    big_top_value_cols = [col for col in data.columns if data[col].value_counts(dropna=False, normalize=True).values[0] > 0.9]
+    return big_top_value_cols
+def get_useless_columns(data):
+    too_many_null = get_too_many_null_attr(data)
+    print("More than 90% null: " + str(len(too_many_null)))
+    too_many_repeated = get_too_many_repeated_val(data)
+    print("More than 90% repeated value: " + str(len(too_many_repeated)))
+    cols_to_drop = list(set(too_many_null + too_many_repeated))
+    cols_to_drop.remove('isFraud')
+    return cols_to_drop
+
+## -------------------
+
+bool_map = {
+    "T":1,
+    "F":0
+}
+
+## -------------------
+
+
